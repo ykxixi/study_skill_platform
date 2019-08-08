93dadafe0bcd930e78774903c8236186ced7efd6
Wangxi <763098805@qq.com>
2019-08-08 22:46:24 +0800

    基本框架

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  mounted() {
+    this.$router.push('/first');
   }
 }
 </script>
 
 <style>
+  html, body {
+    margin: 0;
+    width: 100%;
+    height: 100%;
+    /* background: gray; */
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*color: #2c3e50;*/
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,1 +1,70 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const routes = [
+    {
+        path: '/first',
+        name: '进入页面',
+        component: () => import('./views/user/FirstPage.vue'),
+        meta: {
+            title: 'study&skill'
+        }
+    },
+    {
+        path: '/home',
+        name: '首页',
+        component: () => import('./views/user/Home.vue'),
+        meta: {
+            title: 'study&skill首页'
+        },
+        children:[
+            {
+                path: '/info',
+                name: '信息',
+                component: () => import('./views/user/Info.vue'),
+                meta: {
+                    title: '信息'
+                }
+            },
+            {
+                path: '/course-list',
+                name: '课程列表',
+                component: () => import('./views/user/CourseList.vue'),
+                meta: {
+                    title: '课程商城'
+                }
+            },
+            {
+                path: '/shopping-mall',
+                name: '积分商城',
+                component: () => import('./views/user/ShoppingMall.vue'),
+                meta: {
+                    title: '积分商城'
+                }
+            },
+        ]
+    },
+    {
+        path: '/my-course',
+        name: '学生_我的课程',
+        component: () => import('./views/user/MyCourse'),
+        meta: {
+            title: '我的课程'
+        }
+    },
+    {
+        path: '/user-info',
+        name: '个人信息',
+        component: () => import('./views/user/UserInfo'),
+        meta: {
+            title: '个人信息'
+        }
+    }
+
+];
+export default new Router({
+        routes
+})
 
