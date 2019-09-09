dece4ee33a48c3e665a7fafcfb13b02b8adc63be
LHY <2962796579@qq.com>
2019-09-09 19:44:10 +0800

    RegisterForm & Register

--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -9,7 +9,17 @@
 <!--                </el-steps>-->
 <!--            </el-row>-->
             <el-row style="margin-top: 70px">
-                <RegisterForm style="padding-left: 40px"></RegisterForm>
+                <el-col>
+                    <el-row>
+                        <el-col span="5" offset="8" style="color: white; margin-top: 30px;">
+                            <el-link :underline="false" @click="toR" style="color: cadetblue" href="../../components/LoginForm.vue">已有账户？立即登陆
+                                <i class="el-icon-s-promotion el-icon--right"></i></el-link>
+                        </el-col>
+                    </el-row>
+                    <el-row style="margin-top: 16px">
+                        <RegisterForm style="padding-left: 40px"></RegisterForm>
+                    </el-row>
+                </el-col>
             </el-row>
 <!--            <el-row style="margin-top: 25px">-->
 <!--                <el-col span="3" offset="15">-->
