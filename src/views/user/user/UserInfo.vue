a427b0a49c7a313cfc11f5618c8b32cad4e33af4
ydl8686 <181250178@smail.nju.edu.cn>
2019-09-14 21:22:17 +0800

    lalala

--- src/views/user/user/UserInfo.vue
+++ src/views/user/user/UserInfo.vue
@@ -9,21 +9,22 @@
   <h2>我的优惠券</h2> -->
     <!-- <h2>账户信息</h2> -->
     <!-- <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" :size="70" style="margin-left:100px"></el-avatar> -->
+    <el-container>
+
+    <el-main style="align-items:center">
     <el-upload
     class="avatar-uploader"
     action="https://jsonplaceholder.typicode.com/posts/"
     :show-file-list="false"
     :on-success="handleAvatarSuccess"
-    :before-upload="beforeAvatarUpload" >
+    :before-upload="beforeAvatarUpload">
     <img v-if="imageUrl" :src="imageUrl" class="avatar">
-    <i v-else class="el-icon-plus avatar-uploader-icon">上传头像</i>
+    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
     </el-upload>
-
-    
-
-    <el-form ref="form" :model="form" label-width="80px" style="clear:both">
+    <h4 >点击头像上传</h4>
+    <el-form ref="form" :model="form" label-width="80px" style="align-self:center">
         <el-form-item label="昵称:" style="width:300px">
-            <el-input v-model="form.name"></el-input>
+            <el-input v-model="form.name" placeholder="请输入昵称"></el-input>
         </el-form-item>
         <el-form-item label="性别:" style="width:200px">
             <el-radio-group v-model="form.sex">
@@ -31,18 +32,24 @@
                 <el-radio label="女"></el-radio>
             </el-radio-group>
         </el-form-item>
-        <el-form-item label="积分:" style="width:300px">
-            <el-input v-model="form.points" ></el-input>
+        <el-form-item label="积分:" style="width:200px">
+            <el-input v-model="form.points" :disabled="true"></el-input>
         </el-form-item>
-    </el-form>  
+    </el-form>
+    </el-main>
+
+    <el-footer>
     <el-popover
     placement="top-start"
     title=""
-    width="200"
+    width="15"
     trigger="hover"
-    content="确认保存">
-    <el-button slot="reference">保存信息</el-button>
-  </el-popover>
+    content="确认更新信息">
+    <el-button slot="reference" @click="alert"><i class="el-icon-upload el-icon--right"></i>保存信息</el-button>
+    </el-popover>
+    </el-footer>
+
+    </el-container>
  </div>
 
 
@@ -53,12 +60,11 @@
         data(){
             return{
                 form:{
-                    name:'lalala',
+                    name:'NJUer',
                     sex:'male',
-                    points:'0'
+                    points:'100'
                 },
-                imageUrl: '',
-                visible:false
+                imageUrl: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
             }
         },
         methods:{
@@ -80,6 +86,19 @@
             },
             onSuccess(){
                 this.$message.error('上传成功');
+            },
+            alert(){
+                
+            this.$alert('', '删除成功', {
+            confirmButtonText: '确定',
+            callback: action => {
+                this.$message({
+                type: 'info',
+                message: `action: ${ action }`
+                });
+            }
+            });
+    
             }
         }
     }
@@ -87,17 +106,19 @@
 
 <style scoped>
     .avatar-uploader .el-upload {
-        border: 1px solid #d9d9d9 !important;
-        border-radius: 6px;
-        cursor: pointer;
-        position: relative;
-        overflow: hidden;
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    margin-left: 500px;
+    margin-bottom: 600px;
     }
     .avatar-uploader .el-upload:hover {
-        border-color: #409EFF;
+        border-color: black;
     }
     .avatar-uploader-icon {
-        font-size: 24px;
+        font-size: 28px;
         color: #8c939d;
         width: 178px;
         height: 178px;
@@ -105,8 +126,13 @@
         text-align: center;
     }
     .avatar {
-        width: 218px;
-        height: 178px;
-        display:block;
+        width: 100px;
+        height: 100px;
+        display: block;
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        position: relative;
+        overflow: hidden;
+    
     }
 </style>
